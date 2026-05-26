788b48908c17b01ff2ed3e61d888ceabaa24f45b
flypigarmy <flypigarmy@qq.com>
2026-05-26 14:57:04 +0800

    Custom server: nas.finalshock.com.cn

--- src/config.rs
+++ src/config.rs
@@ -156,8 +156,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["nas.finalshock.com.cn"];
+pub const RS_PUB_KEY: &str = "yBBWlLph9G+FI5t3qyDMnpozQoXoiMDyXYcrSAU0O1A=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
